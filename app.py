97f05ce3f542f193c5df62e6a388e49e75ed3e6c
jaimeaznar <jaimeaznarupv@gmail.com>
2025-03-30 21:08:54 +0200

    update import

--- app.py
+++ app.py
@@ -225,5 +225,5 @@ def get_recommendations():
     return jsonify(ai_recommendations)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5050))
     app.run(host='0.0.0.0', port=port, debug=True)
